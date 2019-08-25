96437f3f8b5180fcc06cc29b9a8e819bbf57ad80
745487524 <745487524@qq.com>
2019-08-26 06:30:26 +0800

    push test demo test

---
+++ src/main/java/com/example/demotypegit/DemotypegitApplication.java
@@ -0,0 +1,13 @@
+package com.example.demotypegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemotypegitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemotypegitApplication.class, args);
+    }
+
+}
